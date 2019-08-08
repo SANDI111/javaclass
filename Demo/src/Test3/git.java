0f40c58de80a3d3ec9d0062d39a2dfad7c846d63
sandeep <sandeept@clavax.us.comgit config --global user.email sandeept@clavax.us.com>
2019-08-08 13:07:38 +0530

    ADD CHANGE2

--- Demo/src/Test3/git.java
+++ Demo/src/Test3/git.java
@@ -1,5 +1,28 @@
 package Test3;
 
+import java.util.Scanner;
+
 public class git {
+	
+	public static void main (String []args)
+	
+	{
+		int a,b;
+		Scanner sc = new  Scanner(System.in);
+		System.out.println("Enter 2 number");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println("Before swapping - a: "+ a +", b: " + b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+	   // System.out.println("after swapping value of +a AND +B");
+	    System.out.println("After swapping  - a: "+ a +", b: " + b);
+		
+		
+		
+		
+	}
+	
 
 }
